231a67c45e934bd2ce61908086fd712abc63ca26
FELIX CHERUIYOT <felix@intasend.com>
2024-10-30 08:25:49 +0300

    Throw error when signature verification fails

--- google-pay/handler_decrypt.go
+++ google-pay/handler_decrypt.go
@@ -42,7 +42,7 @@ func (d *googlePayHandler) Decrypt(input []byte) (*GooglePayToken, error) {
 	}
 
 	if err := req.verifyMessageSignature(d.merchantId); err != nil {
-		//return nil, err
+		return nil, err
 	}
 
 	// derive to get :
